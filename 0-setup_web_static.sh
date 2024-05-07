2467a99487a22229782486fb69bf0b1c44ccb168
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-07 01:54:34 +0100

    add em

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,17 @@
 #!/usr/bin/env bash
-
-# Install Nginx if not already installed
+# Bash scrip to set up web servers to deploy web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo touch /data/web_static/releases/test/index.html
-
-# Create fake HTML content
-echo "<html>
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create symbolic link
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Define the desired Nginx location block
-NGINX_LOCATION_BLOCK="    location /hbnb_static {
-        alias /data/web_static/current/;
-    }"
-
-# Check if the location block for /hbnb_static already exists in the Nginx configuration file
-if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
-    # If it doesn't exist, append it to the configuration file
-    sudo sed -i '/index.html;/a \\n'"$NGINX_LOCATION_BLOCK" /etc/nginx/sites-available/default
-fi
-
-# Restart Nginx
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
-
-exit 0
